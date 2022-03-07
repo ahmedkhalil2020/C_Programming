d163785d65ba1fe88db8f0e131511ea9ced22a47
Dell5570 <Dell5570@192.168.1.7>
2022-03-07 23:31:52 +0200

    A=65 , a=97

---
+++ print the ASCII/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/*
+ *
+ *
+Write a program to print the ASCII value of a character
+input by the user
+*
+*
+*/
+
+
+int main(void)
+{
+
+char Num;
+
+scanf("%c",&Num);
+printf("%d",Num);
+
+return 0;
+}
